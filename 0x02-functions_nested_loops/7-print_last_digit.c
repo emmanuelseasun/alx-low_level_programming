22e9c24ba4b8ab00870a63825a1f90113a587f2b
emmanuelseasun <emmanuelseasun@yahoo.com>
2022-04-12 23:11:58 -0700

    print alphabet

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n:  is the int that will use for the argument of the function
+ * @n: is the int that will use for the argument of the function
  * Return: integer value
  */
 
